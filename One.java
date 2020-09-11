c7859fc19e7c1ac5b2f7e7ba8b0b9128b38fd3d8
neocia <70805398+neocia@users.noreply.github.com>
2020-09-11 17:44:38 -0300

    Add files via upload

---
+++ One.java
@@ -0,0 +1,15 @@
+package Exercicios;
+
+/* Desafio Proposto:
+ * 1) Faça um programa que mostre a mensagem "Olá Mundo" na tela. */
+
+/* Primeiro vamos criar a Classe que terá o nome de "One"*/
+
+public class One {
+	/* em seguida iremos estabelecer o método*/
+	public static void main(String[] args) {
+/* Comando de saída do desafio*/
+		System.out.println("Olá Mundo!");
+	}
+}
+/*FIM*/
